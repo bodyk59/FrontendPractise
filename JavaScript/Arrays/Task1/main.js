441374aac2182c97846300e636c9b25b4a4a84f9
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-06-10 15:58:32 +0300

    Added Arrays

---
+++ JavaScript/Arrays/Task1/main.js
@@ -0,0 +1,17 @@
+let array = [];
+
+array.push('red');
+array.push('orange');
+array.push('yellow');
+array.push('green');
+array.push('blue');
+array.push('indigo');
+array.push('violet');
+
+console.log(array.pop());
+console.log(array.pop());
+console.log(array.pop());
+console.log(array.pop());
+console.log(array.pop());
+console.log(array.pop());
+console.log(array.pop());
---
+++ JavaScript/Arrays/Task3/main.js
@@ -0,0 +1,6 @@
+let purchases = ['potatoes', 'oranges', 'soap', 'shampoo'];
+
+console.log(purchases[3]);
+console.log(purchases[2]);
+console.log(purchases[1]);
+console.log(purchases[0]);
