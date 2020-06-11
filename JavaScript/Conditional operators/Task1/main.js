f9538b8afecfe446e89d63751169e44953defa20
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-06-11 08:13:22 +0300

    Added Conditional operators

---
+++ JavaScript/Conditional operators/Task1/main.js
@@ -0,0 +1,9 @@
+let age = 18;
+let condition = age < 18;
+
+if (condition) {
+    console.log("Sorry, but you are a minor.");
+}
+else {
+    console.log("That's ok. You can enter the pub.");
+}
---
+++ JavaScript/Conditional operators/Task2/main.js
@@ -0,0 +1,7 @@
+let light = 299792458;
+let sound = 343;
+
+let lightIsPower = light > sound;
+
+console.log(lightIsPower);
+
