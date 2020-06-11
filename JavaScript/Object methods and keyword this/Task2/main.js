a942e23cbfb30c6d2929bcb53e7c75606a298148
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-06-11 08:14:27 +0300

    Added Object methods and keyword this

---
+++ JavaScript/Object methods and keyword this/Task2/main.js
@@ -0,0 +1,9 @@
+let box = {
+    length: 20,
+    width: 30,
+    height: 15,
+
+    getVolume() {
+        return this.length * this.width * this.height;
+    }
+};
---
+++ JavaScript/Object methods and keyword this/Task3/main.js
@@ -0,0 +1,10 @@
+let car = {
+    speed: 372,
+    accelerate() {
+        return this.speed + 5;
+    },
+    brake() {
+        return this.speed - 5;
+    }
+
+};
